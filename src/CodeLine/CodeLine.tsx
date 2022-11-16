49aaaaaa34c84c153bd81fdaf07f63e899c63c30
Erik Tallang <et@computas.com>
2022-11-16 22:26:59 +0100

    feat: improve randomization of code line fragments

--- src/CodeLine/CodeLine.tsx
+++ src/CodeLine/CodeLine.tsx
@@ -5,16 +5,24 @@ import css from "./CodeLine.module.scss";
 export const CodeLine: React.FC = () => {
   const [lineParts, setLineParts] = useState<number[]>([]);
 
-  useEffect(() => {
-    const noOfItems = 6 + Math.round(Math.random() * 3);
-    let list = new Array(noOfItems).fill(1);
-    list = list.map((_, index) => {
-      if (index === 0 || index == noOfItems - 1) {
-        return Math.random() * 7;
+  const createLineParts = (): number[] => {
+    let noOfItems = 6 + Math.round(Math.random() * 3);
+    if (noOfItems % 2 === 0) {
+      noOfItems++;
+    }
+
+    const list = new Array(noOfItems).fill(1).map((_, index) => {
+      if (index === 0 || index === noOfItems - 1) {
+        return 0.2 + Math.random() * 10;
       }
       return 1 + Math.random() * (index % 2 === 0 ? 1 : 8);
     });
-    setLineParts(list);
+
+    return list;
+  };
+
+  useEffect(() => {
+    setLineParts(createLineParts());
   }, []);
 
   return (
