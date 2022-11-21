769ddcb277562407fe7621b16491ccf9c7f8670c
Erik Tallang <et@computas.com>
2022-11-21 20:30:47 +0100

    feat: implement using canvas

---
+++ src/ComputerScreen/CodeLine.ts
@@ -0,0 +1,71 @@
+interface CodeFragment {
+  left: number;
+  width: number;
+}
+
+export class CodeLine {
+  private lineParts: CodeFragment[] = [];
+  private dy = 1;
+
+  constructor(
+    private context: CanvasRenderingContext2D,
+    private y: number,
+    private lineHeight: number,
+    private containerWidth: number
+  ) {
+    this.lineParts = this.createLineParts();
+  }
+
+  createLineParts = (): CodeFragment[] => {
+    let noOfItems = 6 + Math.round(Math.random() * 3);
+    if (noOfItems % 2 === 0) {
+      noOfItems++;
+    }
+
+    const weightedList = new Array(noOfItems).fill(1).map((_, index) => {
+      if (index === 0 || index === noOfItems - 1) {
+        return 0.2 + Math.random() * 10;
+      }
+      return 1 + Math.random() * (index % 2 === 0 ? 1 : 8);
+    });
+
+    const totalWeight = weightedList.reduce((prev, curr) => prev + curr);
+    let offsetLeft = 0;
+
+    return weightedList.map((fragment) => {
+      const width = (fragment / totalWeight) * this.containerWidth;
+      const left = offsetLeft;
+
+      offsetLeft += width;
+
+      return {
+        left: left,
+        width: width,
+      };
+    });
+  };
+
+  update() {
+    this.y -= this.dy;
+    this.renderUI();
+  }
+
+  isOutOfBounds(): boolean {
+    return this.y < -(this.lineHeight / 2);
+  }
+
+  private renderUI() {
+    this.context.lineJoin = "round";
+    this.context.lineWidth = this.lineHeight;
+    this.lineParts.forEach((part, index) => {
+      this.context.beginPath();
+      this.context.moveTo(part.left, this.y);
+      this.context.lineTo(part.left + part.width, this.y);
+      this.context.closePath();
+      this.context.strokeStyle = index % 2 === 0 ? "transparent" : "#579f8e";
+      this.context.shadowColor = "rgb(140 241 218 / 0.2)";
+      this.context.shadowBlur = 10;
+      this.context.stroke();
+    });
+  }
+}
--- src/ComputerScreen/ComputerScreen.tsx
+++ src/ComputerScreen/ComputerScreen.tsx
@@ -1,20 +1,91 @@
 import React, { useEffect, useRef, useState } from "react";
-import { CodeList } from "../CodeList/CodeList";
+import { CodeLine } from "./CodeLine";
 import css from "./ComputerScreen.module.scss";
 
+let lastTimeStamp = 0;
+const fps = 30;
+const lineHeight = 14;
+const padding = 20;
+interface ScreenDimensions {
+  width: number;
+  height: number;
+}
+
 export const ComputerScreen: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const screenRef = useRef<HTMLDivElement>(null);
-  const [numberOfRows, setNumberOfRows] = useState(0);
+  const [screenDimensions, setScreenDimensions] = useState<ScreenDimensions>({
+    width: 0,
+    height: 0,
+  });
+
+  const createCodeLine = (context: CanvasRenderingContext2D): CodeLine => {
+    return new CodeLine(
+      context,
+      screenDimensions.height + lineHeight * 2.6,
+      lineHeight,
+      screenDimensions.width
+    );
+  };
+
+  const render = (
+    ctx: CanvasRenderingContext2D,
+    lines: CodeLine[],
+    timestamp?: number
+  ): void => {
+    window.requestAnimationFrame((time) => render(ctx, lines, time));
+
+    if (timestamp && timestamp - lastTimeStamp > 1000 / fps) {
+      lastTimeStamp = timestamp;
+      ctx?.clearRect(0, 0, screenDimensions.width, screenDimensions.width);
+      if (lines[0].isOutOfBounds()) {
+        lines.splice(0, 1);
+        lines.push(createCodeLine(ctx));
+      }
+      lines.forEach((codeLine) => codeLine.update());
+    }
+  };
+
+  const createCodeLineList = (ctx: CanvasRenderingContext2D): CodeLine[] => {
+    const count =
+      Math.round(screenDimensions.height / (lineHeight + padding)) + 1;
+    return new Array(count)
+      .fill("")
+      .map(
+        (_, index) =>
+          new CodeLine(
+            ctx,
+            (index + 1) * (lineHeight + padding),
+            lineHeight,
+            screenDimensions.width
+          )
+      );
+  };
 
   useEffect(() => {
     if (screenRef.current) {
-      setNumberOfRows(Math.round(screenRef.current.clientHeight / 8));
+      setScreenDimensions({
+        width: screenRef.current.clientWidth - 32,
+        height: screenRef.current.clientHeight,
+      });
     }
-  }, [screenRef, screenRef?.current]);
+  }, [screenRef?.current]);
+
+  useEffect(() => {
+    const context = canvasRef.current?.getContext("2d");
+    if (context && screenDimensions.height && screenDimensions.width) {
+      const lines = createCodeLineList(context);
+      render(context, lines);
+    }
+  }, [screenDimensions]);
 
   return (
     <div className={css["computer-screen"]} ref={screenRef}>
-      <CodeList numberOfRows={numberOfRows} />
+      <canvas
+        ref={canvasRef}
+        width={screenDimensions.width}
+        height={screenDimensions.height}
+      />
     </div>
   );
 };
